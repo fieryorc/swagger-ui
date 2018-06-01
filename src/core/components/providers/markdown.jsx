1d585f3694abb13ab803b7a39d3b687d96179cd2
Prem Ramanathan <premram@google.com>
2018-06-01 00:27:33 -0700

    Disable sanitization in markdown

--- src/core/components/providers/markdown.jsx
+++ src/core/components/providers/markdown.jsx
@@ -8,29 +8,32 @@ import cx from "classnames"
 const isPlainText = (str) => /^[A-Z\s0-9!?\.]+$/gi.test(str)
 
 function Markdown({ source, className = "" }) {
-    if(isPlainText(source)) {
-      // If the source text is not Markdown,
-      // let's save some time and just render it.
-      return <div className="markdown">
+    return <div className="markdown">
         {source}
-      </div>
-    }
-    const html = new Remarkable({
-        html: true,
-        typographer: true,
-        breaks: true,
-        linkify: true,
-        linkTarget: "_blank"
-    }).render(source)
-    const sanitized = sanitizer(html)
+    </div>
+// if(isPlainText(source)) {
+//       // If the source text is not Markdown,
+//       // let's save some time and just render it.
+//       return <div className="markdown">
+//         {source}
+//       </div>
+//     }
+//     const html = new Remarkable({
+//         html: true,
+//         typographer: true,
+//         breaks: true,
+//         linkify: true,
+//         linkTarget: "_blank"
+//     }).render(source)
+//     const sanitized = sanitizer(html)
 
-    if ( !source || !html || !sanitized ) {
-        return null
-    }
+//     if ( !source || !html || !sanitized ) {
+//         return null
+//     }
 
-    return (
-        <div className={cx(className, "markdown")} dangerouslySetInnerHTML={{ __html: sanitized }}></div>
-    )
+//     return (
+//         <div className={cx(className, "markdown")} dangerouslySetInnerHTML={{ __html: sanitized }}></div>
+//     )
 }
 
 Markdown.propTypes = {
